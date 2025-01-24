bf78f69003d563da23455b41a9e722f5955723e5
Zack Williams <73105122+zwill22@users.noreply.github.com>
2025-01-24 12:09:12 +0000

    Added file to clean project directory

---
+++ clean.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+rm -Rf install
+rm -Rf run
+rm -Rf iOSBuild.egg-info
+rm -Rf .vscode
+rm -Rf .venv
+rm -Rf .ruff_cache
+rm -Rf .pytest_cache
+rm -Rf uv.lock
+rm ios.toolchain.cmake
+rm -Rf tests/__pycache__
+rm -Rf ios_build/__pycache__
+rm -Rf .idea
+rm -Rf build/
